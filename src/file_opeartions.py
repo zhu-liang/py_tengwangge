7f42d206abc97709016280910e8993653007317f
Zhu Liang <zlux2357@gmail.com>
2018-07-08 18:54:31 +0800

    1. decorator with two layers which means decorator call receives params

--- src/file_opeartions.py
+++ src/file_opeartions.py
@@ -1,5 +1,7 @@
+#coding:UTF-8
 import json
 from functools import reduce
+
 
 print("hello world")
 
@@ -38,9 +40,56 @@ print(list(filter(lambda x: x%2==0, [1, 2, 3, 4, 5])))
 
 LIST_REF = {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9}
 
-def trans(strA):
+def strToInt(strA):
     return reduce(lambda x, y: x*10+y, map(lambda x:LIST_REF[x], strA))
 
-print(trans('1234'))
+print(strToInt('1234'))
+
+strTest  = "3.1415926"
+print(strTest.index('.'))
+print('7' not in strTest)
+
+#查找字符在串中最后一次出现的地方
+print(strTest.rindex('.'))
+
+#decorator不带参数
+def addMoreInfo(func):
+    def wrapper(*args):
+        print("Before Calling %s"%func.__name__)
+        func(*args)
+        print("After calling %s"%func.__name__)
+
+    return wrapper
+
+#decorator带参数
+def addMoreInfoV2(text):
+    def wrapperL1(func):
+        def wrapperL2(*args):
+            print("V2 Before Calling %s  %s"% (func.__name__, text))
+            func(*args)
+            print("V2 After Calling %s   %s"% (func.__name__, text))
+        return wrapperL2
+    return wrapperL1
+
+#@addMoreInfo
+@addMoreInfoV2('hello')
+def strToFloat(strA):
+    if '.' not in strA:
+        return strToInt()
+    else:
+        dotPos = strA.index('.')
+        first_part= strA[:dotPos]
+        second_part = strA[dotPos+1:]
+        print(first_part + " " + second_part)
+        print(strToInt(first_part) + float(strToInt(second_part))/10**len(second_part))
+
+strToFloat("100.23")
+
+
+
+
+
+
+
 
 
